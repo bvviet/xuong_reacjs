171e3fa24b18fefa87a510b95c9502b0cff2f08c
bvviet <lanviethoang2004@thoang2004gmail.g2004gmailcom>
2024-07-06 12:09:57 +0700

    change server

--- src/App.tsx
+++ src/App.tsx
@@ -3,64 +3,34 @@ import "./App.css";
 
 import ClientLayout from "./Layouts/Client/ClientLayout";
 import HomeClient from "./pages/Client/HomeClient";
-<<<<<<< HEAD
-import DetailClient from "./pages/Client/DetailClient";
 
-function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<ClientLayout />}>
-                <Route index element={<HomeClient />} />
-                <Route index element={<HomeClient />} />
-                <Route path="/detail/:id" element={<DetailClient />} />
-            </Route>
-        </Routes>
-    );
-}
-=======
 import AdminLayout from "./Layouts/admin/AdminLayout";
 import AdminProductList from "./pages/Admin/list";
 import AdminProductAdd from "./pages/Admin/add";
 
 export const routers = createBrowserRouter([
-  {
-    path: "/",
-    element: <ClientLayout />,
-    children: [
-      {
+    {
         path: "/",
-        element: <HomeClient />,
-      },
-    ],
-  },
-  {
-    path: "/admin",
-    element: <AdminLayout />,
-    children: [
-      {
-        path: "list",
-        element: <AdminProductList />,
-      },
-      {
-        path: "add",
-        element: <AdminProductAdd />,
-      },
-    ],
-  },
+        element: <ClientLayout />,
+        children: [
+            {
+                path: "/",
+                element: <HomeClient />,
+            },
+        ],
+    },
+    {
+        path: "/admin",
+        element: <AdminLayout />,
+        children: [
+            {
+                path: "list",
+                element: <AdminProductList />,
+            },
+            {
+                path: "add",
+                element: <AdminProductAdd />,
+            },
+        ],
+    },
 ]);
-// function App() {
-//     return (
-//         <Routes>
-//             <Route path="/" element={<ClientLayout />}>
-//                 <Route index element={<HomeClient />} />
-//             </Route>
-//             <Route path="/admin" element={<AdminLayout />}>
-//                 <Route index element={<AdminProductList/>} />
-//             </Route>
-//         </Routes>
->>>>>>> d905978889196dfad378b2c0db79e9cb03181564
-
-//     );
-// }
-
-// export default App;
