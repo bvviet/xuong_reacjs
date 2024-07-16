59a419928299047af02e8a571c8865143c0bf7d3
tienviet04 <abvutienviet@gmail.com>
2024-07-16 19:52:19 +0700

    done 80%

--- src/Layouts/admin/AdminLayout.tsx
+++ src/Layouts/admin/AdminLayout.tsx
@@ -1,34 +1,54 @@
-import { ThemeProvider } from "@mui/material/styles";
 import { Stack } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles"; // Corrected import
 import { Outlet, useNavigate } from "react-router-dom";
 import Sidebar from "../../components/admin/sideBar/sideBar";
 import { useContext, useEffect } from "react";
 import { UserContext } from "../../contexts/userContext";
+import { LoadingContext } from "../../contexts/LoadingContext";
+import Loading from "../../components/client/Loading/Loading";
+import theme from "../../components/admin/theme/theme";
 
 function AdminLayout() {
-    const { user } = useContext(UserContext);
+  const { user } = useContext(UserContext);
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+  const context = useContext(LoadingContext);
+
+  if (!context) {
+    throw new Error("LoadingContext must be used within a LoadingProvider");
+  }
+
+  const { isLoading } = context;
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/login");
+      return;
+    }
     console.log(user);
 
-    const navigate = useNavigate();
-    const token = localStorage.getItem("token");
-    useEffect(() => {
-        if (!token) {
-            navigate("/login");
-            return;
-        } else if (user?.role !== "admin") {
-            navigate("/");
-            alert("Bạn không có quyền vào trang quản trị");
-            return;
-        }
-    }, [token, navigate]);
-    return (
-        <>
-            <Stack direction={"row"} gap={2}>
-                <Sidebar />
-                <Outlet />
-            </Stack>
-        </>
-    );
+    if (user) {
+      if (user?.role !== "admin") {
+        navigate("/");
+        alert("Bạn không có quyền vào trang quản trị");
+        return;
+      }
+
+      navigate("/admin");
+    }
+  }, [token, user, navigate]);
+
+  return (
+    <>
+      <Loading isShow={isLoading} />
+      <ThemeProvider theme={theme}>
+        <Stack direction={"row"} gap={2}>
+          <Sidebar />
+          <Outlet />
+        </Stack>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default AdminLayout;
--- src/main.tsx
+++ src/main.tsx
@@ -7,15 +7,18 @@ import { ProductsProvider } from "./contexts/ProductsContext.tsx";
 import React from "react";
 import { LoadingProvider } from "./contexts/LoadingContext.tsx";
 import configureAxios from "./configs/axios.tsx";
+import { UserProvider } from "./contexts/userContext.tsx";
 
 axios.defaults.baseURL = "http://localhost:3000";
 configureAxios();
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <ProductsProvider>
-            <LoadingProvider>
-                <RouterProvider router={routers} />
-            </LoadingProvider>
-        </ProductsProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <UserProvider>
+      <ProductsProvider>
+        <LoadingProvider>
+          <RouterProvider router={routers} />
+        </LoadingProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </React.StrictMode>
 );
