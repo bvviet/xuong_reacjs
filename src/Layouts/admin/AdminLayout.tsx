703d21f8c0929ecb99196888b94b77ac7b0bc732
tienviet04 <abvutienviet@gmail.com>
2024-07-20 18:53:56 +0700

    done

--- src/Layouts/admin/AdminLayout.tsx
+++ src/Layouts/admin/AdminLayout.tsx
@@ -1,8 +1,8 @@
 import { Stack } from "@mui/material";
-import { ThemeProvider } from "@mui/material/styles"; // Corrected import
+import { ThemeProvider } from "@mui/material/styles";
 import { Outlet, useNavigate } from "react-router-dom";
 import Sidebar from "../../components/admin/sideBar/sideBar";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../contexts/userContext";
 import { LoadingContext } from "../../contexts/LoadingContext";
 import Loading from "../../components/client/Loading/Loading";
@@ -13,6 +13,7 @@ function AdminLayout() {
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
   const context = useContext(LoadingContext);
+  const [isAuthorized, setIsAuthorized] = useState<boolean | null>(null);
 
   if (!context) {
     throw new Error("LoadingContext must be used within a LoadingProvider");
@@ -22,32 +23,40 @@ function AdminLayout() {
 
   useEffect(() => {
     if (!token) {
-      navigate("/login");
+      setTimeout(() => {
+        alert("Bạn cần đăng nhập để vào admin");
+      }, 500);
+      navigate("/");
       return;
     }
-    console.log(user);
 
     if (user) {
-      if (user?.role !== "admin") {
+      if (user.role !== "admin") {
+        setTimeout(() => {
+          alert("Bạn không có quyền vào trang quản trị");
+        }, 500);
         navigate("/");
-        alert("Bạn không có quyền vào trang quản trị");
-        return;
+      } else {
+        setIsAuthorized(true);
       }
-
-      navigate("/admin");
     }
   }, [token, user, navigate]);
 
+  if (isAuthorized === null) {
+    return <Loading isShow={isLoading} />;
+  }
+
+  if (!isAuthorized) {
+    return null; // Hoặc một component khác nếu bạn muốn
+  }
+
   return (
-    <>
-      <Loading isShow={isLoading} />
-      <ThemeProvider theme={theme}>
-        <Stack direction={"row"} gap={2}>
-          <Sidebar />
-          <Outlet />
-        </Stack>
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme}>
+      <Stack direction={"row"} gap={2}>
+        <Sidebar />
+        <Outlet />
+      </Stack>
+    </ThemeProvider>
   );
 }
 
